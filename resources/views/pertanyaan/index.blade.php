5ec863865aec361c9ea69ab9ce8bb50f9f55e8c7
fikri-hafidzudin <[fikri.hafizuddin@gmail.com]>
2020-08-08 15:41:13 +0700

    ubah h3

--- resources/views/pertanyaan/index.blade.php
+++ resources/views/pertanyaan/index.blade.php
@@ -5,7 +5,7 @@
     
     <div class="card">
     <div class="card-header">
-            <h3 class="card-title">Bordered Table</h3>
+            <h3 class="card-title">Pertanyaan</h3>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
